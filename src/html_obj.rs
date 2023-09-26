7dc2b76f841ad94fbdc5caaa911d38ad6a1ca167
Kārlis Gaigals <karlis@nic.lv>
2023-09-26 20:56:48 +0300

    Removed comments, old stuff and added draw_pixel helper method

--- src/html_obj.rs
+++ src/html_obj.rs
@@ -1,13 +1,5 @@
-// use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{CanvasRenderingContext2d, HtmlElement};
-// use web_sys::window;
 use std::default::Default;
-use std::fmt::format;
-use std::mem;
-use web_sys::console;
-
-// let document:Element;
 
 const CANVAS_CONTAINER_ID:&str = "canvas-container";
 const CANVAS_ID:&str = "canvas-display";
@@ -50,7 +42,6 @@ pub struct Canvas {
     pub container       :Option<web_sys::HtmlElement>,
     canvas              :Option<web_sys::HtmlCanvasElement>,
     pub context_2d      :Option<web_sys::CanvasRenderingContext2d>,
-    test                :u32,
 }
 
 // Implementing the Default trait for Canvas
@@ -60,42 +51,32 @@ impl Default for Canvas {
             container: None,
             canvas: None,
             context_2d: None,
-            test: 0,
         }
     }
 }
 
 impl Canvas {
-    // pub fn new(dom :&Dom) -> Result<Canvas, String> {
-    //     let canvas = Canvas{};
-    //
-    //     let container = find_container(dom)?;
-    //     create_canvas(&container);
-    //     let canvas = select_canvas(dom)?;
-    //     let context_2d = select_context_2d(&canvas)?;
-    //
-    //     Ok(Canvas { container, canvas, context_2d })
-    // }
-
-
     pub fn new(dom :&Dom) -> Result<Canvas, String> {
         let mut canvas_obj = Canvas::default();
 
-        // canvas_obj.container = canvas_obj.find_container(dom)?;
         canvas_obj.find_container(dom)?;
-        console::log_1(&"after".into());
-        create_canvas(&canvas_obj.container.as_ref().unwrap());
-        canvas_obj.canvas = Some(select_canvas(dom)?);
-        // canvas_obj.context_2d = select_context_2d(canvas_obj.canvas?);
-        canvas_obj.context_2d = Some(select_context_2d(canvas_obj.canvas.as_ref().unwrap())?);
+        canvas_obj.create_canvas();
+        canvas_obj.select_canvas(dom)?;
+        canvas_obj.select_context_2d()?;
 
-        // Ok(Canvas { container, canvas, context_2d, test: 1 })
         Ok(canvas_obj)
     }
 
-    fn find_container(&mut self, dom :&Dom) -> Result<(), String> {
-        console::log_1(&"0".into());
+    pub fn size(&mut self) -> (i32, i32) {
+        let container = self.container.as_ref().unwrap();
+        ( container.offset_width(), container.offset_height())
+    }
 
+    pub fn draw_pixel(&mut self, x :f64, y :f64, width :f64, height :f64) {
+        self.context_2d.as_ref().unwrap().fill_rect(x, y, width, height);
+    }
+
+    fn find_container(&mut self, dom :&Dom) -> Result<(), String> {
         let container_element = dom.document.
             get_element_by_id(CANVAS_CONTAINER_ID).
             ok_or("failed to find 'canvas container' element".to_string())?;
@@ -106,82 +87,58 @@ impl Canvas {
             return Err("failed to find 'canvas container' HTML element".to_string());
         }
 
-        let container = container_html_element_result.unwrap();
-
-        console::log_1(&"11".into());
-        console::log_1(&format!{"{:?}\n", container}.into());
-
-        self.container = Some(container);
-        console::log_1(&format!{"{:?}\n", self}.into());
+        self.container = Some(container_html_element_result.unwrap());
         return Ok(())
     }
 
-}
+    fn create_canvas(&mut self) {
+        let ( canvas_width, canvas_height) = self.size();
 
-fn find_container(dom :&Dom) -> Result<HtmlElement, String> {
-    let container_element_result = dom.document.get_element_by_id(CANVAS_CONTAINER_ID);
-    if container_element_result == None {
-        return Err("failed to find 'canvas container' element".to_string());
+        let html:String = format!("<canvas id=\"{CANVAS_ID}\" width=\"{canvas_width}\" height=\"{canvas_height}\"></canvas>");
+
+        let container = self.container.as_ref().unwrap();
+        container.set_inner_html(html.as_str());
     }
 
-    let container_element = container_element_result.unwrap();
+    fn select_canvas(&mut self, dom :&Dom) -> Result<(), String> {
+        let canvas = dom.document.get_element_by_id(CANVAS_ID).
+            ok_or("failed to find 'canvas' element".to_string())?;
 
-    let container_html_element_result = container_element.dyn_into::<web_sys::HtmlElement>();
-    if container_html_element_result.is_err() {
-        return Err("failed to find 'canvas container' HTML element".to_string());
+        // Canvas casting as HTMLElement
+        let canvas_html_element = canvas.dyn_into::<web_sys::HtmlCanvasElement>();
+        if canvas_html_element.is_err() {
+            return Err("failed to find 'canvas' HTML element".to_string());
+        }
+
+        self.canvas = Some(canvas_html_element.unwrap());
+        Ok(())
     }
 
-    Ok(container_html_element_result.unwrap())
-}
+    fn select_context_2d(&mut self) -> Result<(), String> {
+        let canvas = self.canvas.as_ref().unwrap();
+
+        let canvas_context_result = canvas.get_context("2d");
+        if canvas_context_result.is_err() {
+            return Err("failed to find 'canvas' 2d context".to_string());
+        }
+
+        let canvas_context_object_result = canvas_context_result.unwrap();
+        if canvas_context_object_result == None {
+            return Err("failed to obtain 'canvas' object".to_string());
+        }
 
 
-fn create_canvas(container :&web_sys::HtmlElement) {
-    let( canvas_width, canvas_height) = size(container); 
+        let canvas_context_object = canvas_context_object_result.unwrap();
 
-    let html:String = format!("<canvas id=\"{CANVAS_ID}\" width=\"{canvas_width}\" height=\"{canvas_height}\"></canvas>");
-    container.set_inner_html(html.as_str());
-}
+        // Canvas 2D casting as CanvasRenderingContext2d
 
-fn select_canvas(dom :&Dom) -> Result<web_sys::HtmlCanvasElement, String> {
-    let canvas_result = dom.document.get_element_by_id(CANVAS_ID);
-    if canvas_result == None {
-        return Err("failed to find 'canvas' element".to_string());
+        let canvas_context_2d_result = canvas_context_object.
+            dyn_into::<web_sys::CanvasRenderingContext2d>();
+        if canvas_context_2d_result.is_err() {
+            return Err("failed to cast canvas object as context2D".to_string());
+        }
+
+        self.context_2d = Some(canvas_context_2d_result.unwrap());
+        Ok(())
     }
-
-    let canvas = canvas_result.unwrap();
-
-    // Canvas casting as HTMLElement
-    let canvas_html_element_result = canvas.dyn_into::<web_sys::HtmlCanvasElement>();
-    if canvas_html_element_result.is_err() {
-        return Err("failed to find 'canvas' HTML element".to_string());
-    }
-    
-    Ok(canvas_html_element_result.unwrap())
-}
-
-fn select_context_2d(canvas :&web_sys::HtmlCanvasElement) -> Result<web_sys::CanvasRenderingContext2d, String> {
-    let canvas_context_result = canvas.get_context("2d");
-    if canvas_context_result.is_err() {
-        return Err("failed to find 'canvas' 2d context".to_string());
-    }
-
-    let canvas_context_object_result = canvas_context_result.unwrap();
-    if canvas_context_object_result == None {
-        return Err("failed to obtain 'canvas' object".to_string());
-    }
-
-    let canvas_context_object = canvas_context_object_result.unwrap();
-
-    // Canvas 2D casting as CanvasRenderingContext2d
-
-    let canvas_context_2d_result = canvas_context_object.dyn_into::<web_sys::CanvasRenderingContext2d>();
-    if canvas_context_2d_result.is_err() {
-        return Err("failed to cast canvas object as context2D".to_string());
-    }
-
-    Ok(canvas_context_2d_result.unwrap())
-}
-
-pub fn size(container :&web_sys::HtmlElement) -> (i32, i32) {
-    ( container.offset_width(), container.offset_height())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 mod html_obj;
 
 use wasm_bindgen::prelude::*;
-// use wasm_bindgen::JsCast;
-// use web_sys::CanvasRenderingContext2d;
-use html_obj::*;
 use web_sys::console;
-// use log::*;
 
 
 #[wasm_bindgen]
@@ -18,61 +14,20 @@ pub fn init_panic_hook() {
 pub fn main() -> Result<(), JsValue> {
     console::log_1(&"Hello from rust".into());
 
-    let dom = html_obj::Dom::new()?;
-    console::log_1(&"Here".into());
-    let canvas = html_obj::Canvas::new(&dom)?;
-
-
-    // canvas.create_canvas();
-    // log::console_log!("Hello from rust macro");
-    
-
-    // let window:web_sys::Window = web_sys::window().expect("no global `window` exists");
-    // let document:web_sys::Document = window.document().expect("should have a document on window");
-    // let body:web_sys::HtmlElement = document.body().expect("document should have a body");
-
-    // let canvas_container:web_sys::Element = dom.document.get_element_by_id(CANVAS_CONTAINER_ID).unwrap();
-    // let container_html_element:web_sys::HtmlElement = canvas_container
-    //     .dyn_into::<web_sys::HtmlElement>()
-    //     .unwrap();
-
-
-
-    // let html:String = format!("<canvas id=\"{CANVAS_ID}\" width=\"{canvas_width}\" height=\"{canvas_height}\"></canvas>");
-    // container_html_element.set_inner_html(html.as_str());
-
-    // let canvas:web_sys::Element = document.get_element_by_id(CANVAS_ID).unwrap();
-    // let canvas_html_element: web_sys::HtmlCanvasElement = canvas
-    //                     .dyn_into::<web_sys::HtmlCanvasElement>()
-    //                     .map_err(|_| ())
-    //                     .unwrap();
-
-    // let canvas_context:CanvasRenderingContext2d = canvas_html_element.get_context("2d")
-    //                     .unwrap()
-    //                     .unwrap()
-    //                     .dyn_into::<web_sys::CanvasRenderingContext2d>()
-    //                     .unwrap();
+    let dom = html_obj::Dom::new()?
+    let mut canvas = html_obj::Canvas::new(&dom)?;
 
     // Required to adjust new size of canvas offset.
     canvas.context_2d.as_ref().unwrap().scale(1., 1.).unwrap();
 
-    let( canvas_width, canvas_height) = size(canvas.container.as_ref().unwrap());
+    let( canvas_width, canvas_height) = canvas.size();
     
     // Dots left.
-    canvas.context_2d.as_ref().unwrap().fill_rect(0., 0., 2., 2.);
-    canvas.context_2d.as_ref().unwrap().fill_rect(0., canvas_height as f64- 2., 2., 2.);
+    canvas.draw_pixel(0., 0., 2., 2.);
+    canvas.draw_pixel(0., canvas_height as f64- 2., 2., 2.);
     // Dots right;
-    canvas.context_2d.as_ref().unwrap().fill_rect(canvas_width as f64 - 2., 0., 2., 2.);
-    canvas.context_2d.as_ref().unwrap().fill_rect(canvas_width as f64, canvas_height as f64- 2., 2., 2.);
-    
-
-    // let val = dom.document.create_element("p")?;
-    // val.set_inner_html(format!("width:{}, height:{}", 
-    //     canvas_width,
-    //     canvas_height,
-    // ).as_str());
-
-    // dom.body.append_child(&val)?;
+    canvas.draw_pixel(canvas_width as f64 - 2., 0., 2., 2.);
+    canvas.draw_pixel(canvas_width as f64 - 2., canvas_height as f64 - 2., 2., 2.);
 
     Ok(())
 }
